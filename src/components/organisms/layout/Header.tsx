d8bb2022dde3c4eddd6b4f3cc6e870095892bde8
Kadotchi <84917429+Kadotchi@users.noreply.github.com>
2022-06-05 22:11:33 +0900

    ヘッダーの作成(コンポーネント分割)

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,67 +1,41 @@
 import { memo, VFC } from "react";
-import {
-  Box,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerOverlay,
-  Flex,
-  Heading,
-  IconButton,
-  Link,
-  useDisclosure
-} from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+
+import { MenuIconButton } from "../../atoms/button/MenuIconButton";
+import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      align="center"
-      justify="space-between"
-      padding={{ base: 3, md: 5 }}
-    >
-      <Flex align="center" as="a" mr={8} _hover={{ pointer: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-          ユーザー管理アプリ
-        </Heading>
-      </Flex>
+    <>
       <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
         align="center"
-        fontSize="sm"
-        flexGrow={2}
-        display={{ base: "none", md: "flex" }}
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
       >
-        <Box pr={4}>
-          <Link>ユーザー一覧</Link>
-        </Box>
-        <Link>設定</Link>
+        <Flex align="center" as="a" mr={8} _hover={{ pointer: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザー管理アプリ
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>ユーザー一覧</Link>
+          </Box>
+          <Link>設定</Link>
+        </Flex>
+        <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <IconButton
-        aria-label="メニューボタン"
-        icon={<HamburgerIcon />}
-        size="sm"
-        variant="unstyled"
-        display={{ base: "block", md: "none" }}
-        onClick={onOpen}
-      />
-      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">Top</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
-    </Flex>
+      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+    </>
   );
 });
