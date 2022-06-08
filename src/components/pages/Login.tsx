d5b9175853a99664d6a455fc56e3cabcfb86736d
Kadotchi <84917429+Kadotchi@users.noreply.github.com>
2022-06-08 23:59:06 +0900

    ログイン画面にCSSを適用

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -3,7 +3,7 @@ import { memo, VFC } from "react";
 
 export const Logitn: VFC = memo(() => {
   return (
-    <Flex>
+    <Flex align="center" justify="center" height="100vh">
       <Box>
         <Heading as="h1">ユーザー管理アプリ</Heading>
         <Divider />
