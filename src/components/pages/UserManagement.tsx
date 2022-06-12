da211e303f3d7806f88dac851e962d76719a0132
Kadotchi <84917429+Kadotchi@users.noreply.github.com>
2022-06-12 20:07:34 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,20 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
+
 export const UserManegement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      {[...Array(10)].map(() => (
+        <WrapItem>
+          <UserCard
+            imageUrl="https://source.unsplash.com/random"
+            userName="かどっち"
+            fullName="Kotaro Kadoshima"
+          />
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
 });
